4f9b7b940ceb677b49b6d557973bbf64e1c5b663
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-02-10 14:43:01 +0000

    responsive_text

--- daniela-app/app/page.tsx
+++ daniela-app/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={355} // Set the desired height
           style={{ height: "355px", width: "1743px" }}
         />
-        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-orange-50 text-8xl font-bold text-center">
+        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-orange-50 sm:text-4xl  lg:text-6xl xl:text-8xl font-bold text-center">
         Consultas de psicólogia online!
       </div>
         <button className="absolute bottom-14 center-4  text-orange-50 border-2 border-orange-50 px-4 py-2 rounded-sm  hover:bg-orange-50 hover:text-orange-900">
