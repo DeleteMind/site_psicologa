cc7426e7f2392f5b99f980068b45d37d439163bb
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-02-10 12:58:54 +0000

    add_images

--- daniela-app/app/page.tsx
+++ daniela-app/app/page.tsx
@@ -2,6 +2,103 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>ola</h1>
+    <main className="flex flex-col items-center justify-center w-full">
+      <div className="w-full h-full bg-white flex flex-col justify-center items-center">
+        <Image
+          src="/WhatsApp Image 2025-02-09 at 17.26.46.jpeg"
+          alt="Logo"
+          width={400} // Set the desired width
+          height={400} // Set the desired height
+          style={{ height: "355px", width: "1743px" }} // Use responsive layout to maintain aspect ratio
+        />
+      </div>
+      <div className="flex flex-row py-2">
+        <div className="bg-white w-64 h-64">
+          <h1>Sobre mim</h1>
+          <span>
+            Lorem ipsum, dolor srwtgrsdgswrartrerdffsvit amet consectetur
+            adipisicing elit. Nostrum dignissimos incidunt obcaecati nisi eius
+            ipsa nesciunt nam nobis dolore voluptatum quidem maxime dolor, ut
+            eos culpa excepturi odit, temporibus repellendus!
+          </span>
+        </div>
+        <Image src="/DSC_0011.JPG" alt="Logo" width={400} height={400} />
+      </div>
+      Lorem ipsum dolor sit amet consectetur adipisicinLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assuLorem ipsum dolor sit
+      amet consectetur adipisicing elit. Laborum esse assug elit. Laborum esse
+      assumenda labore eaque incidunt ad temporibus quibusdam eligendi, suscipit
+      earum laboriosam, necessitatibus cumque, harum tenetur quaerat repellendus
+      accusantium. Minus, porro?
+    </main>
   );
 }
