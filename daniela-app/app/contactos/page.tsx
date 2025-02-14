dac0e3b6e0b09fdb379cbbc244f414c33c906f48
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-02-14 15:46:33 +0000

    change_about_page

--- daniela-app/app/contactos/page.tsx
+++ daniela-app/app/contactos/page.tsx
@@ -1,7 +1,7 @@
-export default function contactosPage() {
-    return (
-    <div className="text-white text-lg p-4">
-      <h1>Página em manutenção...</h1>
-    </div>
+import Image from 'next/image';
+
+export default function ContactosPage() {
+  return (
+    <h1> Manutenção</h1>
   );
-  }
+}
--- daniela-app/app/sobre/page.tsx
+++ daniela-app/app/sobre/page.tsx
@@ -1,71 +1,33 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function SobrePage() {
   return (
-    <main className="container mx-auto p-6">
-      {/* Sobre mim e imagem */}
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-        <div className="space-y-4 ">
-          <h1 className="text-2xl font-bold">SOBRE MIM</h1>
-          <div className="space-y-2 text-justify">
-            <p>Sou a Daniela Sousa, Psicóloga Clínica, mestre em Psicoterapia Cognitiva - Comportamental e Integrativa, pela Faculdade de Psicologia da Universidade de Lisboa. Interesso-me especialmente pelas relações humanas e pela forma como o nosso sistema nervoso se modifica à medida que vamos criando novas relações interpessoais. </p>
-            <p>Na sala da terapia, importa-me acolher cada pessoa no seu todo, com aceitação e curiosidade para conhecer as suas vulnerabilidades, defesas e recursos. Considero importante ajustar as formas de intervenção às individualidades de cada um, por sermos todos únicos e não gostar de catalogar. É na relação terapêutica que se cria espaço para a consciência dos processos internos e, se potencia a mudança.</p>
-            <p>Dou consultas em Lisboa e Leiria, na tentativa de manter o equilíbrio entre o crescimento e a estabilidade, entre a cidade e o campo.</p>
-            <p>Atualmente frequento a formação avançada em psicoterapia na Associação Portuguesa de Terapias Cognitiva, Comportamental e Integrativa (APTCCI). Paralelamente, tenho vindo a fazer formações que me permitem conhecer melhor a relação entre o corpo e a mente, e entender mais sobre processos interpessoais e a repetição dos mesmos,  tendo especial carinho e interesse por Terapia Focada na Emoções, Terapia Interpessoal Reconstrutiva, e Terapia Somática.</p>
-            <p>Cédula Profissional nº26868 da Ordem dos Psicólogos Portugueses</p>
+    <main className="text-black text-lg flex justify-center p-4 sm:p-12">
+      <section className="flex flex-col lg:flex-row gap-x-16">
+        {/* Title */}
+        <div className="flex flex-col gap-4 sm:gap-16 lg:w-1/2 mb-6">
+          <h1 className="text-4xl sm:text-8xl">Daniela Santos <br /> Sousa</h1>
+          <div className="space-y-2 sm:space-y-4">
+            <h2 className="text-xl sm:text-2xl">Psicóloga Clínica</h2>
+            <p className="text-sm sm:text-base">Cédula Profissional nº26868 da Ordem dos Psicólogos Portugueses</p>
           </div>
         </div>
-        <Image
-          src="/1000052268.jpg"
-          alt="Sobre"
-          width={500}
-          height={1080}
-          className="rounded-lg shadow-md w-full h-auto"
-        />
-      </section>
-
-      {/* Experiencia e imagem */}
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-12">
-        <Image
-          src="/1000052270.jpg"
-          alt="Curriculo"
-          width={500}
-          height={1080}
-          className="rounded-lg shadow-md w-full h-auto"
-        />
-        <div className="space-y-2">
-          <h2 className="text-2xl font-bold">EXPERIÊNCIA PROFISSIONAL</h2>
-          <ul className="">
-            <li>Prática Privada na Psinove - Inovamos a Psicologia;</li>
-            <li>Prática Privada em consultório em Leiria;</li>
-            <li>Experiência na Avaliação Psicológica no âmbito de recrutamento e seleção;</li>
-            <li>Experiência na Orientação Vocacional e de Carreira.</li>
-          </ul>
-        </div>
-      </section>
-
-      {/* Formação */}
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-12">
-        <div className="space-y-2 text-justify">
-          <h3 className="text-2xl font-bold">SÍNTESE CURRICULAR</h3>
-          <ul className="list-">
-            <li>- Mestrado Integrado em Psicologia Clínica, núcleo de Psicoterapia Cognitiva-Comportamental e Integrativa, pela Faculdade de Psicologia da Universidade de Lisboa;</li>
-            <li>- Formação em &quot;Intervenção Psicológica nas Dependências Com e Sem Substâncias&quot;;</li>
-            <li>- Formação em &quot;Intervenção Psicológica no Luto&quot;;</li>
-            <li>- Treino introdutório em Somatic Experiencing;</li>
-            <li>- Participação em workshops, colóquios e conferências: &quot;Workshop Chronic/Recurrent Suicidality: Implementing Interpersonal Reconstructive Therapy for Conceptualization, Safety Planning, & Reconstruction&quot;,&quot;Workshop Interpersonal Reconstructive Therapy for Facilitating Response to Cognitive-Behavioral Therapy with Challenging Cases.&quot;, &quot;Workshop: Therapist Self-care, the Therapeutic Relationships that Work and Therapist Responsiveness&quot;; &quot;Workshop: Case Conceptualization for Personality Disorders: How to Foster Therapist Responsiveness&quot;, &quot;Society for the Exploration of Psychotherapy Integration 35th Annual Conference&quot;;  e &quot;Seminário Fome Emocional e Compulsiva&quot;;</li>
-            <li>- Membro da Society for the Exploration of Psychotherapy Integration;</li>
-            <li>- Formadora Certificada pelo Instituto de Emprego e Formação Profissional (IEFP);</li>
-            <li>- Frequência na Pós-graduação em Psicoterapia pela Associação Portuguesa de Terapias Comportamental, Cognitiva e Integrativa (APTCCI).</li>
-          </ul>
-        </div>
-        <Image
-          src="/1000052260.jpg"
-          alt="Formação"
-          width={500}
-          height={1080}
-          className="rounded-lg shadow-md w-full h-auto" />
-
+        {/* Image and about */}
+        <section className="flex flex-col gap-4 sm:gap-8 w-full lg:w-1/2">
+          <Image
+            src="/Snapinst.app_368680928_270420565750050_196321165440213060_n_1080.jpg"
+            alt="Sobre"
+            width={500}
+            height={1080}
+            className="rounded-lg shadow-md w-full max-w-[500px] h-auto object-cover"
+          />
+          <div className="space-y-2 sm:space-y-4 text-justify">
+            <p className="text-sm sm:text-base">Sou a Daniela Sousa, Psicóloga Clínica, mestre em Psicoterapia Cognitiva - Comportamental e Integrativa, pela Faculdade de Psicologia da Universidade de Lisboa. Interesso-me especialmente pelas relações humanas e pela forma como o nosso sistema nervoso se modifica à medida que vamos criando novas relações interpessoais.</p>
+            <p className="text-sm sm:text-base">Na sala da terapia, importa-me acolher cada pessoa no seu todo, com aceitação e curiosidade para conhecer as suas vulnerabilidades, defesas e recursos. Considero importante ajustar as formas de intervenção às individualidades de cada um, por sermos todos únicos e não gostar de catalogar. É na relação terapêutica que se cria espaço para a consciência dos processos internos e, se potencia a mudança.</p>
+            <p className="text-sm sm:text-base">Dou consultas em Lisboa e Leiria, na tentativa de manter o equilíbrio entre o crescimento e a estabilidade, entre a cidade e o campo.</p>
+            <p className="text-sm sm:text-base">Atualmente frequento a formação avançada em psicoterapia na Associação Portuguesa de Terapias Cognitiva, Comportamental e Integrativa (APTCCI). Paralelamente, tenho vindo a fazer formações que me permitem conhecer melhor a relação entre o corpo e a mente, e entender mais sobre processos interpessoais e a repetição dos mesmos, tendo especial carinho e interesse por Terapia Focada na Emoções, Terapia Interpessoal Reconstrutiva, e Terapia Somática.</p>
+          </div>
+        </section>
       </section>
     </main>
   );
