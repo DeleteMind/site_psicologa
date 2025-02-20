262c2642a6af4a32afab57eafab2df74fe71e3fb
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-02-20 01:06:23 +0000

    change_footer

--- daniela-app/components/Footer.tsx
+++ daniela-app/components/Footer.tsx
@@ -7,26 +7,23 @@ export default function Footer() {
     const year = new Date().getFullYear();
     return (
         <footer>
-            <div className="flex flex-col md:flex-row items-center justify-center w-full bg-gray-500 text-white gap-6 p-4">
-                <div className="flex flex-col gap-2 items-center justify-center">
-                    <h1 className="text-2xl font-bold">Contactos</h1>
-                    <p>Telefone: 913 444 303</p>
-                    <p>Email: danielasousa.psicoterapia@gmail.com</p>
-                </div>
+            <div className="flex flex-col items-center justify-center w-full bg-gray-500 text-white p-4">
                 <div className="flex flex-row justify-center gap-4 items-center">
-                    <h2 className="text-xl md:text-2xl">Redes Socias</h2>
+                <h1 className="text-xl">Redes Sociais |</h1>
                     <a href="https://www.instagram.com/danielasousapsi?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram style={{ height: 30, width: 30 }} />
+                        <FaInstagram style={{ height: 20, width: 20 }} />
                     </a>
                     <a href="https://www.linktr.ee/danielasousapsicologia">
-                        <PiLinktreeLogo style={{ height: 30, width: 30 }} />
+                        <PiLinktreeLogo style={{ height: 20, width: 20 }} />
                     </a>
                     <a href="https://www.linkedin.com/in/daniela-santos-sousa">
-                        <FaLinkedin style={{ height: 30, width: 30 }} />
+                        <FaLinkedin style={{ height: 20, width: 20 }} />
                     </a>
                 </div>
+              
+
             </div>
-            <p className="text-xs flex justify-center w-full bg-white p-2">©2025-{year} by Filipe Cordeiro.</p>
+            <p className="text-xs flex justify-center w-full bg-white p-2">©{year} by Filipe Cordeiro.</p>
         </footer>
     );
 }
