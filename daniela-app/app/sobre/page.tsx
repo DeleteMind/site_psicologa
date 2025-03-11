e5ec04f89998efd4726385274ea9b3833b13bcc4
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-03-11 13:38:09 +0000

    update_sobre_page

--- daniela-app/app/sobre/page.tsx
+++ daniela-app/app/sobre/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 export default function SobrePage() {
   return (
-    <main className="text-black text-lg flex flex-col justify-between min-h-screen p-4 sm:p-12 lg:mx-4 xl:mx-8">
+    <main className="text-black text-lg min-h-screen p-4 sm:p-12 lg:mx-4 xl:mx-8">
       {/* Title and Image */}
-      <div className="flex flex-col lg:flex-row w-full lg:w-3/4 justify-center items-end mx-auto gap-x-16 ">
-        <div className="space-y-2 text-center lg:text-left">
-          <h1 className="text-4xl sm:text-6xl lg:text-8xl text-center lg:text-right">Daniela Sousa</h1>
+      <div className="flex flex-col-reverse md:flex-row w-full lg:w-3/4 justify-start items-center lg:items-end mx-auto gap-x-16">
+        <div className="flex flex-col items-start">
+          <h1 className="text-4xl sm:text-6xl lg:text-8xl text-center lg:text-right mt-8 lg:m-0">Daniela Sousa</h1>
           <div className="pt-8 sm:pt-16 lg:pt-24">
-            <h2 className="mb-4 sm:mb-8 text-lg sm:text-xl lg:text-4xl text-center lg:text-left">Psicóloga Clínica</h2>
-            <p className="text-xs sm:text-sm lg:text-base text-center lg:text-left">Cédula Profissional nº26868 da Ordem dos Psicólogos Portugueses</p>
+            <h2 className="mb-4 sm:mb-8 text-lg sm:text-xl lg:text-4xl">Psicóloga Clínica</h2>
+            <p className="text-xs sm:text-sm lg:text-base">Cédula Profissional nº26868 da Ordem dos Psicólogos Portugueses</p>
           </div>
         </div>
         <Image
@@ -22,10 +22,13 @@ export default function SobrePage() {
       </div>
 
       {/* Section at the bottom */}
-      <section className="mt-10 text-justify text-xs sm:text-sm lg:text-base border-t border-gray-300/50 gap-x-8 grid grid-cols-1 sm:grid-cols-2 pt-8">
-        <p className="mb-4">Sou a Daniela Sousa, Psicóloga Clínica, mestre em Psicoterapia Cognitiva - Comportamental e Integrativa, pela Faculdade de Psicologia da Universidade de Lisboa. Interesso-me especialmente pelas relações humanas e pela forma como o nosso sistema nervoso se modifica à medida que vamos criando novas relações interpessoais.</p>
-        <p className="mb-4">Na sala da terapia, importa-me acolher cada pessoa no seu todo, com aceitação e curiosidade para conhecer as suas vulnerabilidades, defesas e recursos. Considero importante ajustar as formas de intervenção às individualidades de cada um, por sermos todos únicos e não gostar de catalogar. É na relação terapêutica que se cria espaço para a consciência dos processos internos e, se potencia a mudança.</p>
-        <p className="mb-4">Frequento a formação avançada em psicoterapia na Associação Portuguesa de Terapias Cognitiva, Comportamental e Integrativa (APTCCI). Paralelamente, tenho vindo a fazer formações que me permitem conhecer melhor a relação entre o corpo e a mente, e entender mais sobre processos interpessoais, tendo especial interesse pela Terapia Focada na Emoções, Terapia Interpessoal Reconstrutiva, e Terapia Somática.</p>
+      <section className="mt-10 pt-8 text-justify text-xs sm:text-sm lg:text-base border-t border-gray-300/50 gap-x-8">
+        <h3 className="mb-8">SÍNTESE CURRICULAR</h3>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
+          <p className="mb-4 lg:mb-0">Sou a Daniela Sousa, Psicóloga Clínica, mestre em Psicoterapia Cognitiva - Comportamental e Integrativa, pela Faculdade de Psicologia da Universidade de Lisboa. Interesso-me especialmente pelas relações humanas e pela forma como o nosso sistema nervoso se modifica à medida que vamos criando novas relações interpessoais.</p>
+          <p className="mb-4 lg:mb-0">Na sala da terapia, importa-me acolher cada pessoa no seu todo, com aceitação e curiosidade para conhecer as suas vulnerabilidades, defesas e recursos. Considero importante ajustar as formas de intervenção às individualidades de cada um, por sermos todos únicos e não gostar de catalogar. É na relação terapêutica que se cria espaço para a consciência dos processos internos e, se potencia a mudança.</p>
+          <p className="mb-4 lg:mb-0">Frequento a formação avançada em psicoterapia na Associação Portuguesa de Terapias Cognitiva, Comportamental e Integrativa (APTCCI). Paralelamente, tenho vindo a fazer formações que me permitem conhecer melhor a relação entre o corpo e a mente, e entender mais sobre processos interpessoais, tendo especial interesse pela Terapia Focada na Emoções, Terapia Interpessoal Reconstrutiva, e Terapia Somática.</p>
+        </div>
       </section>
     </main>
   );
