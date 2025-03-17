bf7e75a9eff89d4d88d13a66b08625595406ce40
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-03-17 15:34:21 +0000

    add_hover_in_recurosPage

--- daniela-app/app/recursos/page.tsx
+++ daniela-app/app/recursos/page.tsx
@@ -7,28 +7,28 @@ export default function Recursos() {
     <main className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-20 py-8 sm:py-12 md:py-16">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Blogs Section */}
-        <section className="md:col-span-2 flex flex-col gap-6">
+        <section className="md:col-span-2 flex flex-col gap-6 ">
           <h2 className="text-2xl">BLOG</h2>
           <Link href="https://24.sapo.pt/opiniao/artigos/seremos-realmente-todos-um-bocadinho-psicologos-os-psis-e-os-pseudopsis" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
+            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow hover:shadow-lg transition duration-300">
               <img src="https://thumbs.web.sapo.io/?W=775&H=0&png=1&delay_optim=1&webp=1&epic=NzM2QhNt4au+L2O5NSIRhhlx+5IRiCS9ZIRtQYFQzgHqYpR7QyqU/v7puvJbDfiXpw4pNyG7MjEKCR9ZP9K0azEGX9ntuvE+Hp/OFsyOTONt+DI=" alt="Blog 1" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo24: Seremos tos realmente um bocadinho psicólogos? Os psis e os pseudopsis</p>
             </div>
           </Link>
           <Link href="https://24.sapo.pt/opiniao/artigos/bem-te-quero-mal-me-quero-quando-o-nosso-bem-estar-parece-depender-dos-outros" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
+            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow hover:shadow-lg transition duration-300">
               <img src="https://thumbs.web.sapo.io/?W=640&H=320&delay_optim=1&tv=1&crop=center&webp=1&epic=NTQ5ze7vM94yw9Sfr3sb8koknThthSJzwhEZ6SxNB2ARAJaMk7wVKgbT8gOsmJDP364atNj1ypmbna26mJctzh0IDicLE4rkc/AvL/s45hke5FU=" alt="Blog 2" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo24: Bem te quero, mal te quero? Quando o nosso bem estar parece depender dos outros</p>
             </div>
           </Link>
           <Link href="https://lifestyle.sapo.pt/vida-e-carreira/comportamento/artigos/conecte-se-com-o-que-importa-neste-natal" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
+            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow hover:shadow-lg transition duration-300">
               <img src="https://thumbs.web.sapo.io/?W=775&H=0&delay_optim=1&webp=1&epic=YTQ5GcXg+q/aoigpLlsWpxA04oDUsT26e8YTKu7nRJO7c6yRFTd/EdTJijo/cTsLuurhB+iqSXIWG5anqAqva9Oi7sq8n4M09pFW6awGiPnUDRY=" alt="Blog 3" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo Lifestyle: Conecte-se com o que importa neste neste Natal</p>
             </div>
           </Link>
           <Link href="https://lifestyle.sapo.pt/vida-e-carreira/comportamento/artigos/estar-no-aqui-e-no-agora-como-passar-da-critica-a-aceitacao-de-nos-proprios" target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
+            <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow hover:shadow-lg transition duration-300">
               <img src="https://thumbs.web.sapo.io/?W=775&H=0&delay_optim=1&webp=1&epic=ZTFjv5K7FXHXpClOfg54xGuunwcUUg3pMgsV2Z/HxTRblXK0yitw4zb0Uy5gHmeGiJ5G8jJpZ+bPB6T8qcns1SxevIKWLcNW7kanoID4u5MUawY=" alt="Blog 4" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo Lifestyle: Estar no aqui e no agora: como passar da &quot;crítica&quot; à &quot;aceitação&quot; de nós próprios?</p>
             </div>
