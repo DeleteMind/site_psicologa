2b7a2461bc8f210568f275ecc1597957a7bd3d0b
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-03-17 12:47:05 +0000

    add_rel_in_recursos_Page

--- daniela-app/app/recursos/page.tsx
+++ daniela-app/app/recursos/page.tsx
@@ -7,25 +7,25 @@ export default function Recursos() {
         {/* Blogs Section */}
         <section className="md:col-span-2 flex flex-col gap-6">
           <h2 className="text-2xl font-bold">Blog</h2>
-          <Link href="https://24.sapo.pt/opiniao/artigos/seremos-realmente-todos-um-bocadinho-psicologos-os-psis-e-os-pseudopsis" target="_blank">
+          <Link href="https://24.sapo.pt/opiniao/artigos/seremos-realmente-todos-um-bocadinho-psicologos-os-psis-e-os-pseudopsis" target="_blank" rel="noopener noreferrer">
             <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
               <img src="https://thumbs.web.sapo.io/?W=775&H=0&png=1&delay_optim=1&webp=1&epic=NzM2QhNt4au+L2O5NSIRhhlx+5IRiCS9ZIRtQYFQzgHqYpR7QyqU/v7puvJbDfiXpw4pNyG7MjEKCR9ZP9K0azEGX9ntuvE+Hp/OFsyOTONt+DI=" alt="Blog 1" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo24: Seremos tos realmente um bocadinho psicólogos? Os psis e os pseudopsis</p>
             </div>
           </Link>
-          <Link href="https://24.sapo.pt/opiniao/artigos/bem-te-quero-mal-me-quero-quando-o-nosso-bem-estar-parece-depender-dos-outros" target="_blank">
+          <Link href="https://24.sapo.pt/opiniao/artigos/bem-te-quero-mal-me-quero-quando-o-nosso-bem-estar-parece-depender-dos-outros" target="_blank" rel="noopener noreferrer">
             <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
               <img src="/blog2.jpg" alt="Blog 2" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo24: Bem te quero, mal te quero? Quando o nosso bem estar parece depender dos outros</p>
             </div>
           </Link>
-          <Link href="https://lifestyle.sapo.pt/vida-e-carreira/comportamento/artigos/conecte-se-com-o-que-importa-neste-natal" target="_blank">
+          <Link href="https://lifestyle.sapo.pt/vida-e-carreira/comportamento/artigos/conecte-se-com-o-que-importa-neste-natal" target="_blank" rel="noopener noreferrer">
             <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
               <img src="https://thumbs.web.sapo.io/?W=775&H=0&delay_optim=1&webp=1&epic=YTQ5GcXg+q/aoigpLlsWpxA04oDUsT26e8YTKu7nRJO7c6yRFTd/EdTJijo/cTsLuurhB+iqSXIWG5anqAqva9Oi7sq8n4M09pFW6awGiPnUDRY=" alt="Blog 3" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo Lifestyle: Conecte-se com o que importa neste neste Natal</p>
             </div>
           </Link>
-          <Link href="https://lifestyle.sapo.pt/vida-e-carreira/comportamento/artigos/estar-no-aqui-e-no-agora-como-passar-da-critica-a-aceitacao-de-nos-proprios" target="_blank">
+          <Link href="https://lifestyle.sapo.pt/vida-e-carreira/comportamento/artigos/estar-no-aqui-e-no-agora-como-passar-da-critica-a-aceitacao-de-nos-proprios" target="_blank" rel="noopener noreferrer">
             <div className="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
               <img src="https://thumbs.web.sapo.io/?W=775&H=0&delay_optim=1&webp=1&epic=ZTFjv5K7FXHXpClOfg54xGuunwcUUg3pMgsV2Z/HxTRblXK0yitw4zb0Uy5gHmeGiJ5G8jJpZ+bPB6T8qcns1SxevIKWLcNW7kanoID4u5MUawY=" alt="Blog 4" className="w-24 h-24 object-cover rounded-md" />
               <p className="text-lg font-medium">(Psinove) Sapo Lifestyle: Estar no aqui e no agora: como passar da &quot;crítica&quot; à &quot;aceitação&quot; de nós próprios?</p>
