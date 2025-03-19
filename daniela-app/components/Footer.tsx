197e2a3e32859c0ac650c037ea995f63d7ac8ad3
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-03-19 01:03:51 +0000

    change_color_footer

--- daniela-app/components/Footer.tsx
+++ daniela-app/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     const year = new Date().getFullYear();
     return (
         <footer>
-            <div className="flex flex-col items-center justify-center w-full bg-gray-500 text-white p-4">
+            <div className="flex flex-col items-center justify-center w-full bg-[#555e5c]/90 text-white p-4">
                 <div className="flex flex-row justify-center gap-4 items-center">
                 <h1 className="text-xl">Redes Sociais |</h1>
                     <a href="https://www.instagram.com/danielasousapsi?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
