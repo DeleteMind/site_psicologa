3121e3fd5c55cf5774450d1d8cdd21221f2010ab
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-06 21:52:45 +0100

    change_text

--- daniela-app/components/PlanosConsultas.tsx
+++ daniela-app/components/PlanosConsultas.tsx
@@ -1,28 +1,28 @@
 const tiers = [
   {
-    name: 'Presencial',
-    id: 'choose-one',
-    href: '/contactos',
-    local: 'Leiria',
+    name: "Presencial",
+    id: "choose-one",
+    href: "/contactos",
+    local: "Leiria",
     description: "Consultório Ser Mais Pessoa - Psicoterapia.",
     // features: ['25 products', 'Up to 10,000 subscribers', 'Advanced analytics', '24-hour support response time'],
     featured: false,
   },
   {
-    name: 'Presencial',
-    id: 'choose-two',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSeM44IA0jHcO8tckw_yct4gnVM5W85farYUPAp-K0yuqDAhRQ/viewform?usp=header',
-    local: 'Lisboa',
+    name: "Presencial",
+    id: "choose-two",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLSeM44IA0jHcO8tckw_yct4gnVM5W85farYUPAp-K0yuqDAhRQ/viewform?usp=header",
+    local: "Lisboa",
     description: "Psinove - Inovamos a Psicologia.",
     // features: ['25 products', 'Up to 10,000 subscribers', 'Advanced analytics', '24-hour support response time2'],
     featured: false,
   },
   {
-    name: 'Online',
-    id: 'choose-three',
-    href: '#',
-    local: 'Online',
-    description: 'Consultas por videochamada.',
+    name: "Online",
+    id: "choose-three",
+    href: "#",
+    local: "Online",
+    description: "Consultas por videochamada.",
     // features: [
     //   'Unlimited products',
     //   'Unlimited subscribers',
@@ -33,25 +33,23 @@ const tiers = [
     // ],
     featured: true,
   },
-
-]
+];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function TiposConsultas() {
   return (
     <main className="relative isolate  px-6 pt-16 pb-16 lg:px-8">
-
-
       <div className="mx-auto max-w-4xl text-center">
         <p className="mt-2 text-5xl font-semibold tracking-tight text-balance sm:text-6xl">
           Escolhe o melhor caminho para ti
         </p>
       </div>
       <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty sm:text-xl/8">
-        Escolhe o plano que mais adequado, para podermos começar a trabalhar juntos.
+        Seleciona o plano mais adequado para podermos começar a trabalhar
+        juntos.
       </p>
       {/* Boxes */}
       <div className="mx-auto mt-16 sm:mt-20 max-w-lg lg:max-w-4xl grid grid-cols-1 lg:grid-cols-3 items-center gap-8">
@@ -59,32 +57,44 @@ export default function TiposConsultas() {
           <div
             key={tier.id}
             className={classNames(
-              tier.featured ? 'relative bg-orange-100 shadow-2xl' : 'bg-white/90 shadow-sm sm:mx-8 lg:mx-0',
               tier.featured
-                ? ''
+                ? "relative bg-orange-100 shadow-2xl"
+                : "bg-white/90 shadow-sm sm:mx-8 lg:mx-0",
+              tier.featured
+                ? ""
                 : tierIdx === 0
-                  ? 'rounded-3xl'
-                  : 'rounded-3xl',
-              'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 grid-flow-row',
+                ? "rounded-3xl"
+                : "rounded-3xl",
+              "rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 grid-flow-row"
             )}
           >
             <h3
               id={tier.id}
-              className={classNames(tier.featured ? 'text-orange-900/90' : 'text-orange-400', 'text-base/7 font-semibold')}
+              className={classNames(
+                tier.featured ? "text-orange-900/90" : "text-orange-400",
+                "text-base/7 font-semibold"
+              )}
             >
               {tier.name}
             </h3>
             <p className="mt-4 flex items-baseline gap-x-2">
               <span
                 className={classNames(
-                  tier.featured ? '' : '', // Color of the name 
-                  'text-5xl font-semibold tracking-tight',
+                  tier.featured ? "" : "", // Color of the name
+                  "text-5xl font-semibold tracking-tight"
                 )}
               >
                 {tier.local}
               </span>
             </p>
-            <p className={classNames(tier.featured ? '' : '', 'mt-6 text-base/7')}> {/* color of the description*/}
+            <p
+              className={classNames(
+                tier.featured ? "" : "",
+                "mt-6 text-base/7"
+              )}
+            >
+              {" "}
+              {/* color of the description*/}
               {tier.description}
             </p>
             {/* <ul
@@ -111,9 +121,9 @@ export default function TiposConsultas() {
               aria-describedby={tier.id}
               className={classNames(
                 tier.featured
-                  ? 'bg-orange-200 ring-1 ring-orange-300 text-[#555e5c]/80 text-shadow shadow-xs hover:bg-orange-300  focus-visible:outline-orange-600'
-                  : 'ring-1 ring-orange-200 ring-inset text-[#555e5c]/80 hover:ring-orange-300 focus-visible:outline-orange-600',
-                'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10',
+                  ? "bg-orange-200 ring-1 ring-orange-300 text-[#555e5c]/80 text-shadow shadow-xs hover:bg-orange-300  focus-visible:outline-orange-600"
+                  : "ring-1 ring-orange-200 ring-inset text-[#555e5c]/80 hover:ring-orange-300 focus-visible:outline-orange-600",
+                "mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10"
               )}
             >
               Começa hoje
@@ -122,5 +132,5 @@ export default function TiposConsultas() {
         ))}
       </div>
     </main>
-  )
+  );
 }
