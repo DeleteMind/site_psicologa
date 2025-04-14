c3b0250d8e54982a83ba8808017d0a6589911151
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-04-14 12:45:49 +0100

    change_sizeOf_text

--- daniela-app/app/sobre/page.tsx
+++ daniela-app/app/sobre/page.tsx
@@ -26,7 +26,7 @@ export default function SobrePage() {
       {/* Section at the bottom */}
       <section className="mt-10 pt-8  text-justify  border-t-2 gap-x-8">
         <h3 className="mb-2 text-2xl p-4">A PSICÓLOGA QUE SOU</h3>
-        <div className="flex flex-col lg:w-2/3 bg-white/90 rounded-2xl shadow-md p-4 text-xs sm:text-sm lg:text-lg">
+        <div className="flex flex-col lg:w-2/3 bg-white/90 rounded-2xl shadow-md p-4 text-sm sm:text-sm lg:text-lg">
           <p className='mb-4'>Sou a Daniela Sousa, Psicóloga Clínica, mestre em Psicoterapia Cognitiva - Comportamental e Integrativa, pela Faculdade de Psicologia da Universidade de Lisboa. Interesso-me especialmente pelas relações humanas e pela forma como o nosso sistema nervoso se modifica à medida que vamos criando novas relações interpessoais.</p>
           <p className='mb-4'>Na sala da terapia, importa-me acolher cada pessoa no seu todo, com aceitação e curiosidade para conhecer as suas vulnerabilidades, defesas e recursos. Considero importante ajustar as formas de intervenção às individualidades de cada um, por sermos todos únicos e não gostar de catalogar. É na relação terapêutica que se cria espaço para a consciência dos processos internos e, se potencia a mudança.</p>
           <p className='mb-4'>Frequento a formação avançada em psicoterapia na Associação Portuguesa de Terapias Cognitiva, Comportamental e Integrativa (APTCCI). Paralelamente, tenho vindo a fazer formações que me permitem conhecer melhor a relação entre o corpo e a mente, e entender mais sobre processos interpessoais, tendo especial interesse pela Terapia Focada na Emoções, Terapia Interpessoal Reconstrutiva, e Terapia Somática.</p>
